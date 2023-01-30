49cd33e7e50395e4eba5187c2f3d96099354a909
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2023-01-30 17:59:02 +0100

    Update funkcije.php

--- funkcije.php
+++ funkcije.php
@@ -24,3 +24,30 @@ function correctInput ()
     }
 
 }
+
+function aboveZero ()
+{
+
+    if(isset($_POST['submit'])){
+        $red = $_POST['red'];
+        $stupac = $_POST['stupac'];
+
+        if($stupac <= 0 || $red <= 0){
+            return 1 ;
+        }
+    }else{
+        return 2 ;
+    }
+
+}
+
+function inputEmpty ()
+{
+
+    if(empty($_POST['red']) || empty($_POST['stupac']))
+    return 1;
+    else{
+        return 2;
+    }
+
+}
